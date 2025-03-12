6ce613eb04b7720cd413dee5009ad881e057591c
Alejandro Osornio <aoxo.contact@gmail.com>
2025-03-12 13:45:27 -0600

    Create db folder
    With initial usuario, auth, prenda and sube implementations

---
+++ db/3. sube.sql
@@ -0,0 +1,7 @@
+DEFINE TABLE sube TYPE RELATION IN usuario OUT prenda ENFORCED SCHEMAFULL PERMISSIONS NONE;
+
+-- ------------------------------
+-- FIELDS
+-- ------------------------------ 
+
+DEFINE FIELD date ON sube TYPE datetime PERMISSIONS FULL;
