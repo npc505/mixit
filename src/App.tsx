29e94aca345e6af6ac96d16bf8ad6c8483ce8a3d
Alejandro Osornio <aoxo.contact@gmail.com>
2025-03-17 18:56:45 -0600

    Add src/surreal.tsx and improve db client handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import { getDb, Surreal } from './surreal';
 import LandingPage from './views/LandingPage'
 import Login from './views/Login'
 import Register from './views/Register'
@@ -6,14 +8,24 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
+  let [client, setClient] = useState<Surreal>();
+
+  useEffect(() => {
+    const initDB = async () => {
+      let db = await getDb();
+      setClient(db);
+    }
+
+    initDB()
+  }, []);
 
   return (
     <>
  <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login db={client}/>} />
+        <Route path="/register" element={<Register db={client}/>} />
       </Routes>
     </Router>
     </>
--- src/surreal/auth.ts
+++ src/surreal/auth.ts
@@ -1,6 +1,7 @@
 import Surreal from "surrealdb";
 
-export async function login(db:Surreal, username:string, password:string): Promise<void> {
+
+export async function login(db: Surreal, username: string, password: string): Promise<void> {
     try {
       const token = await db.signin({
         access : 'account',
@@ -17,3 +18,53 @@ export async function login(db:Surreal, username:string, password:string): Promi
       console.error("Failed", err instanceof Error ? err.message : String(err));
     }
   }
+
+interface GoogleOauth {
+  username: string,
+  sub: string
+}
+
+interface OwnAuth {
+  username: string,
+  email: string,
+  password: string,
+}
+
+function instanceOfGoogleOauth(object: any): object is GoogleOauth {
+    return 'sub' in object && 'username' in object;
+}
+
+export async function register(db: Surreal, credentials: GoogleOauth | OwnAuth): Promise<void> {
+  try {
+    if (instanceOfGoogleOauth(credentials)) {
+      console.log("Register with Google OAuth")
+      const token = await db.signup({
+          access : 'account',
+          namespace: 'mixit',
+          database: 'mixit',
+          variables: {
+            username: credentials.username,
+            sub: credentials.sub,
+          },
+      });
+      
+      console.log("ahora existe", token);
+    } else {
+      console.log("Register with Email")
+      const token = await db.signup({
+          access : 'account',
+          namespace: 'mixit',
+          database: 'mixit',
+          variables: {
+            username: credentials.username,
+            email: credentials.email,
+            password: credentials.password,
+          },
+      });
+      
+      console.log("ahora existe", token);
+    }
+  } catch (err: unknown) {
+      console.error("Failed", err instanceof Error ? err.message : String(err));
+  }
+}
