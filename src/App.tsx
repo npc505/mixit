b69f2feb85245df27b6e3ba8c884dac6c983553e
Alejandro Osornio <aoxo.contact@gmail.com>
2025-03-27 21:12:01 -0600

    Make the db instance const

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import Details from './views/Details';
-import { getDb, Surreal } from './surreal';
-import LandingPage from './views/LandingPage'
-import Login from './views/Login'
-import Register from './views/Register'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Details from "./views/Details";
+import { getDb, Surreal } from "./surreal";
+import LandingPage from "./views/LandingPage";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  let [client, setClient] = useState<Surreal>();
+  const [client, setClient] = useState<Surreal>();
 
   useEffect(() => {
     const initDB = async () => {
-      let db = await getDb();
+      const db = await getDb();
       setClient(db);
-    }
+    };
 
-    initDB()
+    initDB();
   }, []);
 
   return (
     <>
- <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login db={client}/>} />
-        <Route path="/register" element={<Register db={client}/>} />
-        <Route path="/details" element={<Details />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login db={client} />} />
+          <Route path="/register" element={<Register db={client} />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
