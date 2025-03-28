759444ae92a333867c93bc0d09843618d765cc95
Miranda Galván Medina <85261121+MirandaGalvan@users.noreply.github.com>
2025-03-27 19:14:34 -0600

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Details from './views/Details';
 import LandingPage from './views/LandingPage'
 import Login from './views/Login'
 import Register from './views/Register'
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
     </Router>
     </>
