c4bf3cc7f8f7a678494dfbabc5e8ab0dcc31296d
Alejandro Osornio <aoxo.contact@gmail.com>
2025-04-18 15:05:58 -0600

    Progress on db

--- db/3. sube.sql
+++
@@ -1,7 +0,0 @@
-DEFINE TABLE sube TYPE RELATION IN usuario OUT prenda ENFORCED SCHEMAFULL PERMISSIONS NONE;
-
--- ------------------------------
--- FIELDS
--- ------------------------------ 
-
-DEFINE FIELD date ON sube TYPE datetime PERMISSIONS FULL;
